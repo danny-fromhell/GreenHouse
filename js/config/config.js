415d4c38a40fb049495d54d37f1de780452bd178
danny-fromhell <daniel19lopez54@gmail.com>
2026-05-20 11:45:44 -0600

    Estructura MVC del proyecto

---
+++ js/config/config.js
@@ -0,0 +1,12 @@
+export const APP_CONFIG = {
+  appName: "Green House",
+  plantDataUrl: "./data/plants.json",
+  cacheKey: "greenhouse_plants_cache",
+  themeKey: "greenhouse_theme",
+  authKey: "greenhouse_admin_session",
+  cacheDuration: 1000 * 60 * 10,
+  demoUser: {
+    email: "admin@greenhouse.com",
+    password: "12345678"
+  }
+};
